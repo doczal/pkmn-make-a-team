6782995f181d4fa299c0732f9c3809787358eac4
Rizal <doczal@gmail.com>
2020-01-06 23:50:21 +0800

    Add search icon to SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,20 +2,46 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import searchIcon from 'assets/images/search.svg';
 import { colors } from './styles/styledVars';
 
 const StyledSearch = styled.div`
   overflow: hidden;
   border-radius: 8px;
+  display: flex;
+  position: relative;
+
   input {
     padding: 0.5rem;
     border: none;
-    border-radius: 8px;
-    width: 100%;
+    flex: 1 1 100%;
     border: 1px solid transparent;
+    border-radius: 8px 0 0 8px;
 
     &:focus {
       border: 1px solid ${colors.blue_00};
+      border-right: none;
+
+      & + .searchIcon {
+        border: 1px solid ${colors.blue_00};
+        border-left: none;
+      }
+    }
+  }
+
+  .searchIcon {
+    position: relative;
+    background: white;
+    flex: 0 0 auto;
+    display: flex;
+    padding: 0 0.5rem;
+    align-items: center;
+    border-radius: 0px 8px 8px 0px;
+    border: 1px solid transparent;
+
+    img {
+      width: 20px;
+      height: 20px;
     }
   }
 `;
@@ -30,6 +56,9 @@ const SearchBar = ({ value, onChange, placeholder }) => {
         value={value}
         onChange={onChange}
       />
+      <div className="searchIcon">
+        <img src={searchIcon} alt="Search" />
+      </div>
     </StyledSearch>
   );
 };
